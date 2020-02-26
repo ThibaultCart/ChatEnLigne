6ead6ac5e4f6744e85bf1c77e534326be1e48163
SoftIce <32594805+SoftIcePink@users.noreply.github.com>
2020-02-26 08:50:49 +0100

    Page controller node js + page de chat
    A faire : 
    - Affichage du nom des utilisateurs pour leurs messages
    - Persistance des messages après déconnexion de l'utilisateur (lorsqu'il se reco, ses messages se réaffiche).

---
+++ chat.js
@@ -0,0 +1,63 @@
+// Basic JS server
+var express = require('express')();
+//var app = express();
+var http = require('http').createServer(express);
+// Passing http as a parameter in the implementation of socket io
+var io = require('socket.io')(http);
+
+
+//express.use(express.static(__dirname + 'public'));
+
+// Sends the chat page as the return page
+express.get('/', function(req, res){
+    res.sendFile(__dirname + '/chat.php');
+});
+
+
+// Listener
+http.listen(3000, function(){
+  console.log('listening on *:80');
+});
+
+
+
+io.on('connection', function(socket){
+
+    console.log('a user connected');
+
+    socket.on('connection', function(){
+        io.emit('new user connected');
+    });
+    
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+    });
+});
+
+io.emit('some event', { 
+    someProperty: 'some value', 
+    otherProperty: 'other value' 
+}); // This will emit the event to all connected sockets
+
+
+/*
+
+Améliorations possibles :
+
+- Persistence des messages
+- Utilisateur
+- Annonces : <Utiliseur> a rejoint le chat / quitter le chat
+- Time Stamp
+- Afficher le dernier message (scroll always en bas par défaut)
+- Designe
+
+
+
+*/
--- chat.php
+++ chat.php
@@ -1,6 +1,29 @@
-<?php
-
-echo "CHAT.PHP"
-
-
-?>
+<!doctype html>
+<html>
+  <head>
+    <title>Chat en-ligne</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <script src="/socket.io/socket.io.js"></script>
+    <script src="https://code.jquery.com/jquery-1.11.1.js"></script>
+    <script>
+      $(function () {
+        var socket = io();
+        $('form').submit(function(e){
+          e.preventDefault(); // prevents page reloading
+          socket.emit('chat message', $('#m').val());
+          $('#m').val('');
+          return false;
+        });
+        socket.on('chat message', function(msg){
+            $('#messages').append($('<li>').text(msg));
+        });
+      });
+    </script>
+  </head>
+  <body>
+    <ul id="messages"></ul>
+    <form action="">
+      <input id="m" autocomplete="off" /><button>Send</button>
+    </form>
+  </body>
+</html>
